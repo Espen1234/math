ade58a13272532bf94709c2e4d36eea025d9c75a
Espen1234 <113055494+Espen1234@users.noreply.github.com>
2022-09-19 13:53:44 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    let sum = 0
+    num = num + 5
+    basic.showNumber(sum)
+    product = num * 5
+    power = num ** 2
+    random = 0
+    basic.showNumber(random)
+})
+let random = 0
+let power = 0
+let product = 0
+let num = 0
+num = 5
+basic.forever(function () {
+	
+})
